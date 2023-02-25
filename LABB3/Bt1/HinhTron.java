07e5789a4729bd5417bffe97eb8c406aee644143
KaliGoBrrr <125338466+KaliGoBrrr@users.noreply.github.com>
2023-02-25 18:02:31 +0700

    Add files via upload

---
+++ LABB3/Bt1/HinhTron.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hinhvuonghinhtron;
+
+/**
+ *
+ * @author admin
+ */
+public class HinhTron {
+    private double PI = 3.14;
+    private float duongKinh;
+    private float banKinh=duongKinh/2;
+    private float chuVi;
+    private float dienTich;
+    //---------------------------------------//
+    public double getPI() {
+        return PI;
+    }
+    public void setPI(double PI) {
+        this.PI = PI;
+    }
+    public float getDuongKinh() {
+        return duongKinh;
+    }
+    public void setDuongKinh(float duongKinh) {
+        this.duongKinh = duongKinh;
+    }
+    public float getBanKinh() {
+        return banKinh;
+    }
+    public void setBanKinh(float banKinh) {
+        this.banKinh = banKinh;
+    }
+    public float getChuVi() {
+        return chuVi;
+    }
+    public void setChuVi(float chuVi) {
+        this.chuVi = chuVi;
+    }
+    public float getDienTich() {
+        return dienTich;
+    }
+    public void setDienTich(float dienTich) {
+        this.dienTich = dienTich;
+    }
+}
+
+    
+
+
+    
