fa0eae34026f9cd96939fc5d7cc8a60996e992e1
KaliGoBrrr <125338466+KaliGoBrrr@users.noreply.github.com>
2023-04-03 19:27:10 +0700

    Add files via upload

---
+++ Lab8/src/Cau3/Bmw.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cau3;
+
+/**
+ *
+ * @author admin
+ */
+public class Bmw implements Car {
+    @Override   
+    public void Accelerate(){
+        System.out.println("Accelerate BMW");
+    }
+}
